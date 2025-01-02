d310752fcc6463f8b54f0ebe1540fcc40f76f98a
DilmyPerera <samadiniperera20@gmail.com>
2025-01-03 01:48:42 +0530

    Import useNavigation from React Navigation and PaperProvider from react-native-paper

--- MyAppNavigate/components/AboutUs.js
+++ MyAppNavigate/components/AboutUs.js
@@ -1,3 +1,5 @@
+import { useNavigation } from `@react-navigation/native`;
+import { PaperProvider, Text , Divider , Button } from 'react-native-paper';
 export default function AboutUs(){
     return(
         <>
