a84dc633137cc7b51b71dc491d099c5f9322b533
DilmyPerera <samadiniperera20@gmail.com>
2025-01-03 02:16:50 +0530

    Install react-native-safe-area-context for safe area handling

--- MyAppNavigate/App.js
+++ MyAppNavigate/App.js
@@ -3,13 +3,11 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function App() {
   const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
   );
 }
 
