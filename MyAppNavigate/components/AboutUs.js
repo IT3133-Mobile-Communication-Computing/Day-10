548e85a9defd53fca452f2828079162db3dd85c9
DilmyPerera <samadiniperera20@gmail.com>
2025-01-03 01:49:45 +0530

    Wrap component tree with PaperProvider for theming support

--- MyAppNavigate/components/AboutUs.js
+++ MyAppNavigate/components/AboutUs.js
@@ -3,7 +3,9 @@ import { PaperProvider, Text , Divider , Button } from 'react-native-paper';
 export default function AboutUs(){
     const navigation = useNavigation();
     return(
-        <>
-        </>
+        <PaperProvider>
+
+        </PaperProvider>
+        
     );
 }
