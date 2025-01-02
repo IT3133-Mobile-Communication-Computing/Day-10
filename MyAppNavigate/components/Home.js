de3c45684fa8d86ded10e07e78ca80fd2807e753
DilmyPerera <samadiniperera20@gmail.com>
2025-01-03 03:13:17 +0530

    fix errors

--- MyAppNavigate/components/Home.js
+++ MyAppNavigate/components/Home.js
@@ -1,4 +1,4 @@
-import { useNavigation } from `@react-navigation/native`;
+import { useNavigation } from '@react-navigation/native';
 import { StyleSheet, View, ScrollView} from 'react-native';
 import { PaperProvider, Text , Divider , Button } from 'react-native-paper';
 export default function Home(){
