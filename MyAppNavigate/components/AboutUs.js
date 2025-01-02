a063ad106e6ebe479bc06aea4e28f2c31a4589ad
DilmyPerera <samadiniperera20@gmail.com>
2025-01-03 01:52:41 +0530

    Add ScrollView with View container for scrollable layout

--- MyAppNavigate/components/AboutUs.js
+++ MyAppNavigate/components/AboutUs.js
@@ -1,10 +1,15 @@
 import { useNavigation } from `@react-navigation/native`;
+import { StyleSheet, View, ScrollView} from 'react-native';
 import { PaperProvider, Text , Divider , Button } from 'react-native-paper';
 export default function AboutUs(){
     const navigation = useNavigation();
     return(
         <PaperProvider>
-
+            <ScrollView>
+                <View>
+                    
+                </View>
+            </ScrollView>
         </PaperProvider>
         
     );
