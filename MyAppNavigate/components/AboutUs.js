5553acbf4a0727df2e28f1e5e758b72f2e6df57c
DilmyPerera <samadiniperera20@gmail.com>
2025-01-03 01:49:16 +0530

    Set up navigation object with useNavigation from React Navigation

--- MyAppNavigate/components/AboutUs.js
+++ MyAppNavigate/components/AboutUs.js
@@ -1,6 +1,7 @@
 import { useNavigation } from `@react-navigation/native`;
 import { PaperProvider, Text , Divider , Button } from 'react-native-paper';
 export default function AboutUs(){
+    const navigation = useNavigation();
     return(
         <>
         </>
