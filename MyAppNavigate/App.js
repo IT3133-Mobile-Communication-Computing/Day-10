dea6e1562bfa878093e5c7ca619e3998899bc0dd
DilmyPerera <samadiniperera20@gmail.com>
2025-01-03 02:22:34 +0530

    Set up stack navigator with screens for home, contact, and about us

--- MyAppNavigate/App.js
+++ MyAppNavigate/App.js
@@ -2,20 +2,30 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Home from './components/Home';
+import ContactUs from './components/ContactUs';
+import AboutUs from './components/AboutUs';
 
 
 export default function App() {
   const Stack = createNativeStackNavigator();
   return (
-    
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='home'
+          screenOptions={{
+            headerStyle :{ backgroundColor : '#e2bee2' },
+          }}
+          >
+            <Stack.Screen name='home' component={Home} options={{title: "Welcome"}} />
+            <Stack.Screen name='contact' component={ContactUs} options={{title: "Contact Us"}} />
+            <Stack.Screen name='aboutus' component={AboutUs} options={{title: "About Us"}} />
+
+          </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
